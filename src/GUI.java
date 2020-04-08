745812c49410a9bed0ff8ce02b7a126a9edfe628
WallFacer5 <zyh9715@126.com>
2020-04-08 01:55:08 -0400

    project UI finished

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,13 @@
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.TextAlignment;
-
-import javax.security.auth.Refreshable;
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.Vector;
 
 public class GUI {
@@ -19,7 +17,7 @@ public class GUI {
     static GridBagLayout grids = null;
 
     public static void main(String[] args) {
-        jf = new JFrame("Dr. Undefined)");
+        jf = new JFrame("Dr. Undefined");
         grids = new GridBagLayout();
         GridBagConstraints gridConstraints = new GridBagConstraints();
         jf.setLayout(grids);
@@ -27,7 +25,7 @@ public class GUI {
         gridConstraints.fill = GridBagConstraints.BOTH;
         gridConstraints.gridx = 1;
         gridConstraints.gridy = 0;
-        gridConstraints.weightx = 0.0;
+        gridConstraints.weightx = 0.25;
         gridConstraints.weighty = 0.1;
         gridConstraints.gridheight = 1;
         gridConstraints.gridwidth = 1;
@@ -40,7 +38,7 @@ public class GUI {
         gridConstraints.fill = GridBagConstraints.BOTH;
         gridConstraints.gridx = 1;
         gridConstraints.gridy = 1;
-        gridConstraints.weightx = 0.0;
+        gridConstraints.weightx = 0.25;
         gridConstraints.weighty = 0.7;
         gridConstraints.gridheight = 1;
         gridConstraints.gridwidth = 1;
@@ -52,12 +50,12 @@ public class GUI {
         gridConstraints.fill = GridBagConstraints.BOTH;
         gridConstraints.gridx = 0;
         gridConstraints.gridy = 0;
-        gridConstraints.weightx = 0.0;
+        gridConstraints.weightx = 0.75;
         gridConstraints.weighty = 0.0;
         gridConstraints.gridheight = 3;
         gridConstraints.gridwidth = 1;
-        String[][] emptyData = new String[50][3];
-        String[] name = {"", "", ""};
+        String[][] emptyData = new String[50][5];
+        String[] name = {"", "", "", "", ""};
         JTable jt = new JTable(emptyData, name);
         jt.setBorder(BorderFactory.createLineBorder(Color.gray));
         jt.setGridColor(Color.gray);
@@ -69,7 +67,7 @@ public class GUI {
         gridConstraints.fill = GridBagConstraints.BOTH;
         gridConstraints.gridx = 1;
         gridConstraints.gridy = 2;
-        gridConstraints.weightx = 0.0;
+        gridConstraints.weightx = 0.25;
         gridConstraints.weighty = 0.2;
         gridConstraints.gridheight = 1;
         gridConstraints.gridwidth = 1;
@@ -78,7 +76,7 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String command = ta.getText();
-                System.out.println(command);
+                // System.out.println(command);
                 ResultSet results = connector.query(command);
                 RefreshTable(results, gridConstraints);
             }
@@ -86,7 +84,7 @@ public class GUI {
         grids.setConstraints(tb, gridConstraints);
         jf.add(tb);
 
-        jf.setBounds(300, 300, 900, 600);
+        jf.setBounds(300, 300, 1200, 600);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -97,45 +95,78 @@ public class GUI {
             String[] headers = new String[col_len];
             Vector<String[]> data = new Vector<String[]>();
             for (int i = 1; i <= col_len; i++) {
-                headers[i-1] = results.getMetaData().getColumnName(i);
+                headers[i - 1] = results.getMetaData().getColumnName(i);
             }
             while (results.next()) {
                 String[] current = new String[col_len];
                 for (int i = 1; i <= col_len; i++) {
-                      current[i-1] = String.format("%s", results.getString(i));
+                    current[i - 1] = String.format("%s", results.getString(i));
                 }
                 data.add(current);
             }
-            System.out.println(1);
             jf.remove(sp);
             gridConstraints.fill = GridBagConstraints.BOTH;
             gridConstraints.gridx = 0;
             gridConstraints.gridy = 0;
-            gridConstraints.weightx = 0.0;
+            gridConstraints.weightx = 0.75;
             gridConstraints.weighty = 0.0;
             gridConstraints.gridheight = 3;
             gridConstraints.gridwidth = 1;
 
             String[][] dataList = new String[data.size()][col_len];
-            for(int i=0; i<data.size(); i++){
+            for (int i = 0; i < data.size(); i++) {
                 dataList[i] = data.get(i);
             }
 
             JTable jt = new JTable(dataList, headers);
-            jt.setAutoResizeMode(jt.AUTO_RESIZE_OFF);
+
+            //jt.setAutoResizeMode(jt.AUTO_RESIZE_OFF);
             jt.setBorder(BorderFactory.createLineBorder(Color.gray));
             jt.setGridColor(Color.gray);
             sp = new JScrollPane(jt);
+            sp.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    Dimension containerWidth = null;
+                    containerWidth = sp.getSize();
+                    int allwidth = jt.getIntercellSpacing().width;
+                    for (int i = 0; i < jt.getColumnCount(); i++) {
+                        int max = 0;
+                        for (int j = 0; j < jt.getRowCount(); j++) {
+                            int width = jt.getCellRenderer(j, i)
+                                    .getTableCellRendererComponent(jt, jt.getValueAt(j, i), false, false, j, i)
+                                    .getPreferredSize().width;
+                            if (width > max) {
+                                max = width;
+                            }
+                        }
+
+                        int headerWidth = jt.getTableHeader().getDefaultRenderer()
+                                .getTableCellRendererComponent(jt, jt.getColumnModel().getColumn(i).getIdentifier(),
+                                        false, false, -1, i).getPreferredSize().width;
+                        if(max < headerWidth){
+                            max = headerWidth;
+                        }
+                        jt.getColumnModel().getColumn(i).setPreferredWidth(max);
+                        allwidth += max + jt.getIntercellSpacing().width;
+                    }
+                    allwidth += jt.getIntercellSpacing().width;
+                    if (allwidth > containerWidth.width) {
+                        jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+                    } else {
+                        jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                    }
+                }
+            });
+
             grids.setConstraints(sp, gridConstraints);
             jf.add(sp);
-            System.out.println(2);
             jf.setVisible(false);
             jf.setVisible(true);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(jf, "SQL error! Please check your input command!",
+                    "SQL error", 0);
             e.printStackTrace();
         }
-
-
     }
 }
